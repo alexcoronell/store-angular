9eeeef0e507f38c02691eac4b5d3b3821a185037
Alex Coronell <alexander@catapultsteel.com>
2024-06-22 14:26:06 -0500

    Layout created, completed and applied on pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,19 @@
 import { Routes } from '@angular/router';
 
+import { LayoutComponent } from '@shared/components/layout/layout.component';
+
 import { ListComponent } from '@products/pages/list/list.component';
 import { AboutComponent } from '@info/pages/about/about.component';
 import { NotFoundComponent } from '@info/pages/not-found/not-found.component';
 
 export const routes: Routes = [
-  { path: '', component: ListComponent },
-  { path: 'about', component: AboutComponent },
+  {
+    path: '',
+    component: LayoutComponent,
+    children: [
+      { path: '', component: ListComponent },
+      { path: 'about', component: AboutComponent },
+    ]
+  },
   { path: '**', component: NotFoundComponent, pathMatch: 'full' },
 ];
